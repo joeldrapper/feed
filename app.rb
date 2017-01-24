8667be90ded7d33a3d06da4abff641c53f0f3d1a
Joel Drapper <joel@drapper.me>
2017-01-24 16:33:29 +0000

    CORS headers

--- app.rb
+++ app.rb
@@ -12,6 +12,8 @@ get '/v1' do
   url = URI.parse params[:url]
 
   content_type :json
+  headers \
+    "Access-Control-Allow-Origin": "*"
 
   cache.fetch url do
     response = HTTParty.get url, format: :xml
