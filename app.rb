d929065c96c1a559774279576051bfb6a2e1cb99
Joel Drapper <joel@drapper.me>
2017-01-25 10:06:24 +0000

    Improved versioning

--- app.rb
+++ app.rb
@@ -8,13 +8,13 @@ cache = Readthis::Cache.new(
   redis: { url: ENV.fetch('REDIS_URL') }
 )
 
-get '/' do
+get '/v1' do
   content_type :json
   headers "Access-Control-Allow-Origin" => "*"
 
   url = URI.parse request.query_string
 
-  cache.fetch url do
+  cache.fetch "v1/#{url}" do
     response = HTTParty.get url, format: :xml
     response.parsed_response.to_json
   end
